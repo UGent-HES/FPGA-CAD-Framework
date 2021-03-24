17c8509bf0fb29fdaf753d6b3cc1689229ed656a
Thijs Paelman <thijs.paelman+git@hotmail.be>
2021-03-24 22:46:45 +0100

    WIP: refactor BoundingBox

--- src/route/route/BoundingBox.java
+++ src/route/route/BoundingBox.java
@@ -9,10 +9,7 @@ class BoundingBox {
 	public short y_max;
 	public BoundingBox() {}
 	public BoundingBox(short uniformRange) {
-		this.x_min = uniformRange;
-		this.x_max = uniformRange;
-		this.y_min = uniformRange;
-		this.y_max = uniformRange;
+		this(uniformRange, uniformRange, uniformRange, uniformRange);
 	}
 	public BoundingBox(short x_min, short x_max, short y_min, short y_max) {
 		this.x_min = x_min;
@@ -20,11 +17,26 @@ class BoundingBox {
 		this.y_min = y_min;
 		this.y_max = y_max;
 	}
+	// copy constructor
+	public BoundingBox(BoundingBox original) {
+		this(original.x_min, original.x_max, original.y_min, original.y_max);
+	}
+
 	public void expand(int uniformRange) {
 		short v = (short) uniformRange;
-		this.x_min += v;
-		this.x_max += v;
-		this.y_min += v;
-		this.y_max += v;
+		this.expand(new BoundingBox(v));
+	}
+	public BoundingBox expand(BoundingBox bbRange) {
+		// bbRange : 	each integer represents how much the bounding box expands at that side.
+		// 				Negative integers represent shrinking.
+		this.x_min -= bbRange.x_min;
+		this.x_max += bbRange.x_max;
+		this.y_min -= bbRange.y_min;
+		this.y_max += bbRange.y_max;
+		return this;
+	}
+	public BoundingBox add(BoundingBox bbRange) {
+		BoundingBox bb = new BoundingBox(this);
+		return bb.expand(bbRange);
 	}
 }
