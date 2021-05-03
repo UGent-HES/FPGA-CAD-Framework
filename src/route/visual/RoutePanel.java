a140892a2d0b74a0c182151f3563bf8c7e04d157
jankan001 <vandaelepieterjan@gmail.com>
2021-05-04 01:22:32 +0200

    drawGrid

--- src/route/visual/RoutePanel.java
+++ src/route/visual/RoutePanel.java
@@ -1,8 +1,9 @@
-package route.visual
+package route.visual;
 
 import route.circuit.architecture.BlockCategory;
 import route.circuit.block.GlobalBlock;
 import route.main.Logger;
+import route.circuit.resource.RouteNode;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -66,7 +67,7 @@ public class RoutePanel extends JPanel {
     			this.setDimensions();
     			this.drawGrid(g);
     			this.drawWires(g);
-    			if (this.mouseEnabled)this.drawBlockInformation(g);
+    			//if (this.mouseEnabled)this.drawBlockInformation(g);
     		} else {
     			this.drawPlot(g);
     		}
@@ -86,6 +87,14 @@ public class RoutePanel extends JPanel {
     	//double maxbbcost = 0.0; for (double bbCost:this.bbCost){maxbbcost = Math.max(bbCost, maxbbcost);}
     	
     	
+    }
+    
+    private void drawLine(Graphics g, double x1, double y1, double x2, double y2) {
+    	g.drawLine((int)Math.round(x1), (int)Math.round(y1), (int)Math.round(x2), (int)Math.round(y2));
+    }
+    
+    private void drawString(Graphics g, String s, double x, double y){
+    	g.drawString(s, (int)Math.round(x), (int)Math.round(y));
     }
     
     private void setDimensions() {
@@ -111,7 +120,24 @@ public class RoutePanel extends JPanel {
     	g.setColor(this.gridColorLight);
     	for (int x = this.left; x <= this.right; x += this.blockSize) {
     		if (x == this.left || x == this.right) {
-    			g.drawLine(this.top + this.blockSize, x, this.bottom - this.blockSize, y);
+    			g.drawLine(x, this.top + this.blockSize, x, this.bottom - this.blockSize);
+    		} else if ((x-this.left)/this.blockSize % 10 == 1) {
+    			g.setColor(this.gridColorDark);
+    			g.drawLine(x, this.top, x, this.bottom);
+    			g.setColor(this.gridColorLight);
+    		} else {
+    			g.drawLine(x, this.top, x, this.bottom);
+    		}	
+    	}
+    	for (int y = this.top; y <= this.bottom; y += this.blockSize) {
+    		if (y == this.top || y == this.bottom) {
+    			g.drawLine(this.left + this.blockSize, y, this.right - this.blockSize, y);
+    		} else if ((y-this.top)/this.blockSize % 10 == 1) {
+    			g.setColor(this.gridColorDark);
+    			g.drawLine(this.left, y, this.right, y);
+    			g.setColor(this.gridColorLight);
+    		} else {
+    			g.drawLine(this.left, y, this.right, y);
     		}
     	}
     }
@@ -122,5 +148,69 @@ public class RoutePanel extends JPanel {
     	//}
     }
     
+    private void drawWire(RouteNode wire, Graphics g) {
+    	//do the goddamn drawing you know
+    }
     
+    //let's just deactivate mouseComponent for a while, since it doesn't work and isn't high-priority
+    /*
+    private void drawBlockInformation(Graphics g) {
+    	final MouseLabelComponent mouseLabel = new MouseLabelComponent(this);
+        //add component to panel
+        this.add(mouseLabel);
+        mouseLabel.setBounds(0, 0, this.getWidth(), this.getHeight());
+        this.addMouseMotionListener(new MouseMotionAdapter(){
+        	public void mouseMoved (MouseEvent me){
+        		mouseLabel.x = me.getX();
+        		mouseLabel.y = me.getY();
+        	}
+        });
+    }
+    
+  	private class MouseLabelComponent extends JComponent{
+  	  	//x , y are from mouseLocation relative to real screen/JPanel
+  	  	//coorX, coorY are FPGA's
+  		private static final long serialVersionUID = 1L;
+  		private int x;
+  		private int y;
+  		private RoutePanel panel;
+  			
+  		public MouseLabelComponent(RoutePanel panel){
+  			this.panel = panel;
+  		}
+  			
+  		protected void paintComponent(Graphics g){
+  			this.drawBlockCoordinate(this.x, this.y, g);
+  		}
+  		
+  		public void drawBlockCoordinate(int x, int y, Graphics g){
+  	    	int coorX = (int)(x-this.panel.left)/this.panel.blockSize;
+  	    	int coorY = (int)(y-this.panel.top)/this.panel.blockSize;
+  	    	if(this.onScreen(coorX, coorY)){
+  	    		String s = "[" + coorX + "," + coorY + "]";
+  	    		GlobalBlock globalBlock = this.getGlobalBlock(coorX, coorY);
+  	    		if(globalBlock != null){
+  	    			s += " " + globalBlock.getName();
+  	    		}
+  	        	int fontSize = 20;
+  	      		g.setFont(new Font("TimesRoman", Font.BOLD, fontSize));
+  	    		g.setColor(Color.BLUE);
+  	    		g.drawString(s, x, y);
+  	    	}
+  	    }
+  	  	public GlobalBlock getGlobalBlock(int x, int y){
+  	        for(Map.Entry<GlobalBlock, Coordinate> blockEntry : this.panel.placement.blocks()) {
+  	        	Coordinate blockCoor = blockEntry.getValue();
+  	        	
+  	        	if(Math.abs(blockCoor.getX() - x) < 0.25 && Math.abs(blockCoor.getY() - y) < 0.25){
+  	        		return blockEntry.getKey();
+  	        	}
+  	        }
+  	        return null;      
+  	    }
+  	    public boolean onScreen(int x, int y){
+  	    	return (x > 0 && y > 0 && x < this.panel.placement.getWidth()+2 && y < this.panel.placement.getHeight()+2);
+  	    }
+  	}
+  	*/
 }
